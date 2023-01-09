bd82f046d3a660fdaaa145f94784fea46e0d828e
dongkyunkimdev <ddkds66@gmail.com>
2023-01-09 18:38:10 +0900

    initial commit

---
+++ src/test/java/com/example/filedb/FiledbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiledbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
