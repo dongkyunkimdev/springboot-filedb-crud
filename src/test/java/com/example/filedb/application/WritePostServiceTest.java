137767ea916d3b52d0f8af4eb4af24a34b3372d3
dongkyunkimdev <ddkds66@gmail.com>
2023-01-09 23:26:23 +0900

    post 작성 application service의 성공하는 단위 테스트 케이스 작성

--- src/test/java/com/example/filedb/application/WritePostServiceTest.java
+++ src/test/java/com/example/filedb/application/WritePostServiceTest.java
@@ -1,13 +1,15 @@
 package com.example.filedb.application;
 
 import com.example.filedb.application.dto.WritePostCommand;
+import com.example.filedb.application.dto.WritePostInfo;
 import com.example.filedb.application.exception.AlreadyExistsPostException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -35,5 +37,22 @@ class WritePostServiceTest {
         assertThrows(AlreadyExistsPostException.class, () -> {
             writePostService.command(duplicatedTitleCommand);
         });
+    }
+
+    @Test
+    void post_작성_성공() {
+        // given
+        WritePostCommand correctCommand = new WritePostCommand("title", "content");
+        given(mockPostPersistencePort.isExistsPostByTitle(correctCommand.getTitle())).willReturn(false);
+        given(mockPostPersistencePort.save(any())).willReturn(correctCommand.toEntity());
+
+        // when
+        WritePostInfo info = writePostService.command(correctCommand);
+
+        // then
+        assertAll(
+                () -> assertEquals(correctCommand.getTitle(), info.getTitle()),
+                () -> assertEquals(correctCommand.getContent(), info.getContent())
+        );
     }
 }
