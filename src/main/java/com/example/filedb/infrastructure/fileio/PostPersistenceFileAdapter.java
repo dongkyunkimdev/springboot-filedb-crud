2879bb181ef7054056119031682ccd780c3c4361
dongkyunkimdev <ddkds66@gmail.com>
2023-01-09 19:51:51 +0900

    char 기반 stream인 file writer와 성능 향상을 위해 buffered writer 객체를 사용하도록 변경

--- src/main/java/com/example/filedb/infrastructure/fileio/PostPersistenceFileAdapter.java
+++ src/main/java/com/example/filedb/infrastructure/fileio/PostPersistenceFileAdapter.java
@@ -4,17 +4,20 @@ import com.example.filedb.application.PostPersistencePort;
 import com.example.filedb.domain.Post;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Component
 public class PostPersistenceFileAdapter implements PostPersistencePort {
     @Override
     public Post save(Post post) {
-        String path = "/filedb/post/" + post.getTitle() + ".txt";
+        String path = "./" + post.getTitle() + ".txt";
         File newFile = new File(path);
 
-        try(FileWriter fw = new FileWriter(newFile)) {
-            fw.write(post.getContent());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(newFile))) {
+            bw.write(post.getContent());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
