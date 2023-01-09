7017615d63a6e6308fbc261b13b0f631293c21e5
dongkyunkimdev <ddkds66@gmail.com>
2023-01-09 19:05:56 +0900

    post 도메인 엔티티 생성자 및 getter 추가

--- src/main/java/com/example/filedb/domain/Post.java
+++ src/main/java/com/example/filedb/domain/Post.java
@@ -1,6 +1,14 @@
 package com.example.filedb.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Post {
     private String title;
     private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
